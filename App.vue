fdf61b4ee6e0a1b07eba0aa36131db3765e76e48
santeng <316096469@qq.com>
2022-05-28 00:19:23 +0800

    vuetest

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyButtonVue msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+import MyButtonVue from './components/MyButton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyButtonVue
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/MyButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+<h2>我爱中国</h2>
+<button @click="count++">爱了{{count}}</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() { 
+    return {
+        count:0
+    }
+  },
+  components:{
+  },
+  mounted() {
+
+  },
+  methods:{
+
+  },
+ }
+</script>
+
+<style scoped>
+</style>
